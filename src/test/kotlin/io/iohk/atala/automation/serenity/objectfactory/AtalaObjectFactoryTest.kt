96a34cf04a758fb9516a730e5c269981dfa3382e
Allain Magyar <allain.magyar@iohk.io>
2023-09-14 13:44:30 -0300

    test: add unit test

--- src/test/kotlin/io/iohk/atala/automation/serenity/objectfactory/AtalaObjectFactoryTest.kt
+++ src/test/kotlin/io/iohk/atala/automation/serenity/objectfactory/AtalaObjectFactoryTest.kt
@@ -1,15 +1,24 @@
 package io.iohk.atala.automation.serenity.objectfactory
 
 import io.cucumber.core.exception.CucumberException
+import io.iohk.atala.automation.WithMockServer
+import io.iohk.atala.automation.extensions.ResponseTest
+import io.iohk.atala.automation.extensions.get
+import net.serenitybdd.rest.SerenityRest
+import net.serenitybdd.screenplay.Actor
+import net.serenitybdd.screenplay.rest.abilities.CallAnApi
+import net.serenitybdd.screenplay.rest.interactions.Get
+import org.hamcrest.CoreMatchers
 import org.hamcrest.CoreMatchers.containsString
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.CoreMatchers.notNullValue
+import org.hamcrest.MatcherAssert
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Assert
 import org.junit.Test
 import javax.inject.Inject
 
-class AtalaObjectFactoryTest {
+class AtalaObjectFactoryTest: WithMockServer() {
     object ObjectTestClass
     private class PrivateTestClass
     class ParameterizedTestClass(val parameter: String)
@@ -63,5 +72,15 @@ class AtalaObjectFactoryTest {
     fun `AtalaObjectFactory should able to create new instance`() {
         val test = AtalaObjectFactory.getInstance(TestClass::class)
         assertThat(test, notNullValue())
+    }
+
+    @Test
+    fun `Should parse OffsetDateTime when using AtalaObjectFactory`() {
+        AtalaObjectFactory
+        val actor = Actor.named("tester").whoCan(CallAnApi.at("http://localhost"))
+        actor.attemptsTo(Get.resource("/offsetdatetime"))
+        val date = SerenityRest.lastResponse().get<ResponseTest.Date>()
+        assertThat(date, notNullValue())
+        assertThat(date.date.toString(), equalTo("2023-09-14T11:24:46.868625Z"))
     }
 }
